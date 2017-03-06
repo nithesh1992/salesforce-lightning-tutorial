3eade57713a54ae15a4756572cebcec8ef996934
Jeff Lombard <jeffl>
2017-03-06 08:51:30 -0800

    Starting Point

--- src/aura/ActiveAccounts/ActiveAccountsHelper.js
+++ src/aura/ActiveAccounts/ActiveAccountsHelper.js
@@ -1,5 +1,29 @@
 ({
 	loadAccounts : function(component) {
+        var helperTextString = "Hello!";
+
+        console.log("Helper",helperTextString);
+
+        var action = component.get("c.getAccountDisplayApexFunction");
+
+        action.setParams({
+            textString : helperTextString,
+        });
+
+        action.setCallback(this,function(response){
+
+            var state = response.getState();
+            var answer = response.getReturnValue();
+
+            if(state === "SUCCESS"){
+                // Gets Evaluated if No Errors.
+                console.log("Callback",answer);
+
+            }
+
+        });
+
+        $A.enqueueAction(action);
 
 	}
 })
